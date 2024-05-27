5c2e0d4f6b4b2e7ae55c06ee4814d0b8eeca388f
Md Shahinur Khan <64772153+shahinurdev@users.noreply.github.com>
2024-05-27 23:49:48 +0600

    Add initial project files

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,20 +1,15 @@
-/* eslint-disable no-undef */
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC5l9cQWB13gMOvlYhch_AdJVeoHpOMtz8",
-  authDomain: "worhard-2b9f9.firebaseapp.com",
-  projectId: "worhard-2b9f9",
-  storageBucket: "worhard-2b9f9.appspot.com",
-  messagingSenderId: "262690927868",
-  appId: "1:262690927868:web:23d239fece71f1f4331379"
+  apiKey: import.meta.env.apiKey,
+  authDomain: import.meta.env.authDomain,
+  projectId: import.meta.env.projectId,
+  storageBucket: import.meta.env.storageBucket,
+  messagingSenderId: import.meta.env.messagingSenderId,
+  appId:import.meta.env.appId
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
